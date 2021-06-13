d5aaf146dd5ba3dd85afe60e04fb209144755562
Calvin Mozola <calvin@stuntfactory.com>
2021-06-12 22:24:25 -0400

    nav

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 import Drawer from '@material-ui/core/Drawer'
+import Button from '@material-ui/core/Button'
 import MenuIcon from '@material-ui/icons/Menu'
 import IconButton from '@material-ui/core/IconButton'
 import CloseIcon from '@material-ui/icons/Close'
 import Grid from '@material-ui/core/Grid'
 
 import brandplh from '../images/brandplh.png'
+
+import '../stylesheets/sidebar.sass'
 
 export default function Sidebar({ activeTab }) {
     const [openVar, setOpen] = React.useState(false)
@@ -16,16 +20,15 @@ export default function Sidebar({ activeTab }) {
             <IconButton onClick={() => setOpen(true)} style={{ color: 'white' }}><MenuIcon /></IconButton>
             <Drawer anchor={'left'} open={openVar}  onClose={() => setOpen(false)}>
                 <div
-                    style={{ width: 341 }}
+                    className='drawer'
                     onClick={() => setOpen(false)}
                 >
                     <Grid container style={{width: "100%"}}>
                         <Grid item xs={12}><IconButton onClick={() => setOpen(false)} style={{ color: '#4DCCBD' }}><CloseIcon /></IconButton></Grid>
-                        <Grid item xs={12} justify="center"><img src={brandplh} /></Grid>
-                        <Grid item xs={12}>Home</Grid>
-                        <Grid item xs={12}>Gallery</Grid>
-                        <Grid item xs={12}>My Purchases</Grid>
-                        <Grid item xs={12}>About Me</Grid>
+                        <Grid item xs={12}><img src={brandplh} /></Grid>
+                        <Grid item xs={12}><Link to='/'>Home</Link></Grid>
+                        <Grid item xs={12}><Link to='/gallery'>Gallery</Link> </Grid>
+                        <Grid item xs={12}><Link to='/bio'>Bio</Link> </Grid>
                     </Grid>
 
                 </div>
--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,31 +1,52 @@
-import React from 'react'
+import React from "react";
 
-import InfoCard from '../components/InfoCard'
+import InfoCard from "../components/InfoCard";
+import SideBar from "../components/Sidebar";
 
 // Material ui
-import Grid from '@material-ui/core/Grid'
-import Button from '@material-ui/core/Button';
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
 
 const gallery = () => {
+  return (
+    <>
+      <SideBar></SideBar>
+      <div
+        className="lg-text bold-text cyan-text"
+        style={{ width: "100%", textAlign: "center" }}
+      >
+        Gallery
+      </div>
+      <br />
+      {[0, 1, 2, 3].map((x) => (
+        <Grid
+          key={x}
+          container
+          spacing={4}
+          justify="center"
+          style={{ padding: "10px 20px" }}
+        >
+          <Grid item xs={12} lg={x % 2 === 0 ? 4 : 6}>
+            <InfoCard />
+          </Grid>
+          <Grid item xs={12} lg={x % 2 === 0 ? 6 : 4}>
+            <InfoCard />
+          </Grid>
+        </Grid>
+      ))}
+      <br />
+      <Grid container justify="center">
+        <Grid item xs={12} md={6}>
+          <Button
+            variant="contained"
+            style={{ backgroundColor: "#D6FFF6", width: "100%" }}
+          >
+            Load more
+          </Button>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
 
-    return (
-        <>
-            <div className="lg-text bold-text cyan-text" style={{ width: '100%', textAlign: 'center' }}>Gallery</div>
-            <br />
-            {[0, 1, 2, 3].map((x) => <Grid key={x} container spacing={4} justify="center" style={{padding: "10px 20px"}}>
-                <Grid item xs={12} lg={x % 2 === 0 ? 4 : 6}><InfoCard /></Grid>
-                <Grid item xs={12} lg={x % 2 === 0 ? 6 : 4}><InfoCard /></Grid>
-            </Grid>)}
-            <br/>
-            <Grid container justify="center">
-                <Grid item xs={12} md={6}>
-                    <Button variant="contained" style={{ backgroundColor: '#D6FFF6', width: '100%' }}>
-                        Load more
-                    </Button>
-                </Grid>
-            </Grid>
-        </>
-    )
-}
-
-export default gallery
+export default gallery;
