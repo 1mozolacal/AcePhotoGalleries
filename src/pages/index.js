de9e645ca9fa14b1f84f8c59405bdcd42180fa52
Duc Nguyen <nahz3112@gmail.com>
2021-08-16 22:56:56 -0400

    Added transition

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,13 @@ import Sidebar from "../components/Sidebar";
 
 // Material ui import
 import Grid from "@material-ui/core/Grid";
+import Grow from '@material-ui/core/Grow'
+import Slide from '@material-ui/core/Slide';
+
 
 import featuredImage from '../images/gallery/THE FAMOUS MOULIN ROUGE IN DOWNTOWN PARIS FRANCE UNREAL SITE.jpg'
 import logo from '../images/tempLogo.png'
 // SVG
-import Photographer from "../images/photographer.svg";
-import Camera from "../images/camera.svg";
 import "../stylesheets/index.sass";
 import "../stylesheets/home.sass";
 // markup
@@ -19,26 +20,36 @@ const IndexPage = () => {
       <Grid container spacing={2} alignItems="center" justify="center" direction="row">
         <Grid item lg={3}>
           <div className="home-context">
-            <div className="logo-holder">
-              <img className="logo" src={logo}></img>
-            </div>
-            <div className="lg-text cyan-text bold-text">            
-              MICHAEL J. MAJOR
-            </div>
-            <div className="md-text blue-text bold-text">
-              Professional Photographer 
-            </div>
-            <hr
-              style={{ height: 3, backgroundColor: "#4DCCBD", border: "none" }}
-            />
-            <div className="lg-text light-cyan-text">
-              Specializing primarily in nature,architecture, and outdoor photography.
-            </div>
+            <Slide in={true} timeout={1000}>
+              <div className="logo-holder">
+                <img className="logo" src={logo}></img>
+              </div>
+            </Slide>
+            <Slide in={true} direction="right" timeout={1000}>
+              <div>
+                <div className="lg-text cyan-text bold-text">
+                  MICHAEL J. MAJOR
+                </div>
+                <div className="md-text blue-text bold-text">
+                  Professional Photographer
+                </div>
+                <hr
+                  style={{ height: 3, backgroundColor: "#4DCCBD", border: "none" }}
+                />
+                <div className="lg-text light-cyan-text">
+                  Specializing primarily in nature,architecture, and outdoor photography.
+                </div>
+              </div>
+            </Slide>
           </div>
         </Grid>
         <Grid item lg={1}></Grid>
         <Grid item lg={7}>
-          <img className="featured" src={featuredImage} alt="Featured image"></img>
+          <Grow in={true} timeout={1000}>
+            <div>
+              <img className="featured" src={featuredImage} alt="Featured image"></img>
+            </div>
+          </Grow>
         </Grid>
       </Grid>
     </main>
