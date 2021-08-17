86c324e6559354c06e13581abbf08bdc9d25c35b
Duc Nguyen <nahz3112@gmail.com>
2021-08-17 10:23:47 -0400

    Fixed paypal btn template

--- src/components/PaypalBtn.jsx
+++ src/components/PaypalBtn.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+// import TextField from '@material-ui/core/TextField';
 
 const sizes = [
     "8X10",
@@ -6,6 +7,13 @@ const sizes = [
     "16X20",
     "20X24",
     "20X30"
+]
+
+const materials = [
+    "Poster",
+    "Acrylic",
+    "Metal",
+    "Stretched Canvas"
 ]
 
 const PaypalBtn = ({ paypalID, prices = [] }) => {
@@ -19,10 +27,43 @@ const PaypalBtn = ({ paypalID, prices = [] }) => {
             <input id="sizes" type="hidden" name="on0" value="Sizes (inches)" />
 
             <select name="os0">
+            {
+                prices.length > 0 ?
+                    prices.map((val, i) => <option key={i} value={sizes[i]}>
+                        {sizes[i]} {val} CAD
+                    </option>)
+                    : null
+            }
+            </select>
+            {/* Styling might be changed */}
+
+            {/* <TextField
+                id="outlined-select-currency-native"
+                select
+                label="Native select"
+                SelectProps={{
+                    native: true,
+                }}
+                helperText="Please select your preferred Size (inches)"
+                variant="outlined"
+                name="os0"
+            >
                 {
-                    prices.length > 0 ?
-                        prices.map((val, i) => <option key={i} value={sizes[i]}>
-                            {sizes[i]} {val} CAD
+                prices.length > 0 ?
+                    prices.map((val, i) => <option key={i} value={sizes[i]}>
+                        {sizes[i]} {val} CAD
+                    </option>)
+                    : null
+                }
+            </TextField> */}
+
+            {/* <label htmlFor="print-type">Print type</label> */}
+            <input type="hidden" id="print-type" name="on1" value="Print type" />
+            <select name="os1">
+                {
+                    materials.length > 0 ?
+                        materials.map((val, i) => <option key={i} value={val}>
+                            {val}
                         </option>)
                         : null
                 }
