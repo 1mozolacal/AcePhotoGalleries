8415b5dad3b549959d1284b8e5a8a01e70d5299c
Calvin Mozola <calvin@stuntfactory.com>
2021-08-17 19:45:23 -0400

    added last of re-modified btn/pic

--- src/utils/galleryConfigs.js
+++ src/utils/galleryConfigs.js
@@ -15,7 +15,7 @@ import merry_christmas_tree from '../images/gallery/LIGHTED CHRISTMAS TREE AT XM
 import lone_seagull from '../images/gallery/LONE SEAGULL ON RAILING WITH DETROIT RIVER IN BACKGROUND.jpg'
 import biodome_a_la_montreal from "../images/gallery/MONTREAL S BIODOME WONDER OF THE WORLD.jpg"
 import river_rapids from '../images/gallery/River Rapids.jpg'
-//import mercury_filter_truck_approaching_sunset from ''
+import mercury_filter_truck_approaching_sunset from '../images/gallery/SPECIAL FILTER SUNSET ON ROAD.jpg'
 import remarkable_colored_sun from '../images/gallery/SPECIAL FILTER SUNSET ON ROAD.jpg'
 import view_along_the_river from '../images/gallery/VIEW DOWN GRAND RIVER TOWARDS CENTURY OLD CHURCH.jpg'
 import willow_tree_in_waiting from '../images/gallery/Willow TREE IN WAITING.jpg'
@@ -65,6 +65,21 @@ import gorgeous_cenote_on_the_gulf_of_mexico from '../images/gallery/GORGEOUS CE
 import times_square_ny_city from '../images/gallery/TIMES SQUARE STREET SHOT.jpg'
 import the_famous_moulin_rouge from '../images/gallery/THE FAMOUS MOULIN ROUGE IN DOWNTOWN PARIS FRANCE UNREAL SITE.jpg'
 
+import long_shot_of_rail_bridge from '../images/gallery/LONG SHOT VIEW OF RAIL BRIDGE OVER GRAND RIVER IN CAMBRIDGE ON.jpg'
+import picturesque_river_through_the_trees from '../images/gallery/SPECIAL FILTER VIEW OF GRAND RIVER AND FOREGROUND FOLIAGE (2).jpg';
+import wooden_bridge_along_water from '../images/gallery/AMAZING WOODEN WALKING TRAIL ALONGSIDE GRAND RIVER WITH SPECIAL FILTER.jpg';
+import arc_de_triomphe from '../images/gallery/ARC DE TRIOMPHE IN PARIS FRANCE.jpg';
+import la_louvre_museum_painted_art_ceiling from '../images/gallery/INCREDIBLE SHOT OF PAINTED CEILING OF THE LOUVRE ART MUSEUM THAT HOUSES LA MONA LISA.jpg';
+import under_the_base_of_the_famous_eiffel_tower_in_paris from '../images/gallery/SHOT UP THE MIDDLE OF THE EIFFEL TOWER AT NIGHT IN PARIS FRANCE.jpg';
+import the_grotto from '../images/gallery/ENTRANCE TO GROTTO CAVE IN TOBERMORY ONTARIO WOW LOOK AT THE WATER COLORS.jpg';
+import new_york_subway from '../images/gallery/NY SUBWAY TRACK 22.jpg';
+import rail_bridge_with_train from '../images/gallery/TRAIN CROSSING RAIL BRIDGE OVER GRAND RIVER.jpg';
+import old_log_dump_side_of_the_grotto from '../images/gallery/THE OTHER SIDE OF THE GROTTO IN TOBERMORY.jpg';
+import the_eiffel_tower_lit_at_night_in_paris_france from '../images/gallery/ONE OF A KIND PICTURE OF PARIS FRANCE S EIFFEL TOWER LIGHTED AT NIGHT FROM BEHIND SECURITY BARRIERS.jpg';
+import beachfront_restaurant_in_mexican_riviera from '../images/gallery/PLAYA DEL CARMEN OCEANSIDE RESTAURANT AND BEACH.jpg';
+import centuries_old_highschool_two from '../images/gallery/HOGWARTS LOOKALIKE GCI SCHOOL IN GALT ONTARIO WITH SPECIAL FILTER.jpg';
+import sunrise_in_agawa_two from '../images/gallery/SUNSET IN AGAWA CANYON BEACH.jpg';
+
 
 export const imageDictionary = {
 	"beautiful yellow flowers": beautiful_yellow_flowers,
@@ -83,7 +98,7 @@ export const imageDictionary = {
 	"lone seagull": lone_seagull,
 	"biodome a la montreal": biodome_a_la_montreal,
 	"river rapids": river_rapids,
-	// "mercury filter truck approaching sunset":mercury_filter_truck_approaching_sunset,
+	"mercury filter truck approaching sunset":mercury_filter_truck_approaching_sunset,
 	"remarkable colored sun": remarkable_colored_sun,
 	"view along the river": view_along_the_river,
 	"willow tree in waiting": willow_tree_in_waiting,
@@ -106,32 +121,47 @@ export const imageDictionary = {
 	"french capitol building": french_capitol_building,
 	"fallen tree in the lake": fallen_tree_in_the_lake,
 	//SECOND BATCH
-	"hiram_walkers_head_office_building": hiram_walkers_head_office_building,
-	"mexican_oasis_and_gardens": mexican_oasis_and_gardens,
-	"agawa_mountainside_waterfall": agawa_mountainside_waterfall,
-	"mill_destroyed_on_the_grand": mill_destroyed_on_the_grand,
-	"old_fishing_camp_on_the_river": old_fishing_camp_on_the_river,
-	"mill_destroyed": mill_destroyed,
-	"leviathan_at_versailles": leviathan_at_versailles,
-	"black_and_white_river_view": black_and_white_river_view,
-	"louis_the_14th_statue_in_paris_france": louis_the_14th_statue_in_paris_france,
-	"mayan_tiki_huts": mayan_tiki_huts,
-	"winter_wonderland": winter_wonderland,
-	"canadian_house_of_commons_historic_architecture": canadian_house_of_commons_historic_architecture,
-	"b_w_of_underbelly_of_ambassdor_bridge": b_w_of_underbelly_of_ambassdor_bridge,
-	"estuary_at_singing_sands_beach": estuary_at_singing_sands_beach,
-	"majestic_ceiling_of_louvre_museum": majestic_ceiling_of_louvre_museum,
-	"extraordinary_stained_glass_in_house_of_commons": extraordinary_stained_glass_in_house_of_commons,
-	"light_tunnel": light_tunnel,
-	"trail_along_the_shore": trail_along_the_shore,
-	"central_park_in_autumn": central_park_in_autumn,
-	"rustic_barn": rustic_barn,
-	"rocky_shoreline_of_bruce_peninsula": rocky_shoreline_of_bruce_peninsula,
-	"pagoda_plantation_in_beijing_china": pagoda_plantation_in_beijing_china,
-	"paris_france_boulevard": paris_france_boulevard,
+	"hiram walkers head office building": hiram_walkers_head_office_building,
+	"mexican oasis and gardens": mexican_oasis_and_gardens,
+	"agawa mountainside waterfall": agawa_mountainside_waterfall,
+	"mill destroyed on the grand": mill_destroyed_on_the_grand,
+	"old fishing camp on the river": old_fishing_camp_on_the_river,
+	"mill destroyed": mill_destroyed,
+	"leviathan at versailles": leviathan_at_versailles,
+	"black and white river view": black_and_white_river_view,
+	"louis the 14th statue in paris france": louis_the_14th_statue_in_paris_france,
+	"mayan tiki huts": mayan_tiki_huts,
+	"winter wonderland": winter_wonderland,
+	"canadian house of commons historic architecture": canadian_house_of_commons_historic_architecture,
+	"b w of underbelly of ambassdor bridge": b_w_of_underbelly_of_ambassdor_bridge,
+	"estuary at singing sands beach": estuary_at_singing_sands_beach,
+	"majestic ceiling of louvre museum": majestic_ceiling_of_louvre_museum,
+	"extraordinary stained glass in house of commons": extraordinary_stained_glass_in_house_of_commons,
+	"light tunnel": light_tunnel,
+	"trail along the shore": trail_along_the_shore,
+	"central park in autumn": central_park_in_autumn,
+	"rustic barn": rustic_barn,
+	"rocky shoreline of bruce peninsula": rocky_shoreline_of_bruce_peninsula,
+	"pagoda plantation in beijing china": pagoda_plantation_in_beijing_china,
+	"paris france boulevard": paris_france_boulevard,
 	"gorgeous cenote on the gulf of mexico": gorgeous_cenote_on_the_gulf_of_mexico,
+
 	"times square ny city": times_square_ny_city,
 	"the famous moulin rouge": the_famous_moulin_rouge,
+	"long shot of rail bridge":long_shot_of_rail_bridge,
+	"picturesque river through the trees":picturesque_river_through_the_trees,
+	"wooden bridge along water":wooden_bridge_along_water,
+	"arc de triomphe":arc_de_triomphe,
+	"la louvre museum painted art ceiling":la_louvre_museum_painted_art_ceiling,
+	"under the base of the famous eiffel tower in paris":under_the_base_of_the_famous_eiffel_tower_in_paris,
+	"the grotto":the_grotto,
+	"new york subway":new_york_subway,
+	"rail bridge with train":rail_bridge_with_train,
+	"old log dump side of the grotto":old_log_dump_side_of_the_grotto,
+	"the eiffel tower lit at night in paris france":the_eiffel_tower_lit_at_night_in_paris_france,
+	"beachfront restaurant in mexican riviera":beachfront_restaurant_in_mexican_riviera,
+	"centuries old highschool two":centuries_old_highschool_two,
+	"sunrise in agawa two":sunrise_in_agawa_two,
 }
 
 export const displaySettingsInitial = [
@@ -149,13 +179,12 @@ export const displaySettingsInitial = [
 	["lazy river", 6, "FREE FLOWING SWAMPY RIVER IN PROVINCIAL PARK"],
 	["centuries old highschool", 6, "GCI School looking like Hogwarts"],
 	["golden gates entrance to palais royale", 6, "Golden Gates to Government's Palais Royale in Paris France"],
-	//Remove because dup issue ["centuries old highschool",6, "Hogwarts Lookalike GCI School with filter"],
 	["canada moose in lights", 6, "Light Show Canada  Moose"],
 	["merry christmas tree", 6, "LIGHTED CHRISTMAS TREE AT XMAS LIGHT SHOW"],
 	["lone seagull", 6, "Lone Seagull on Railing with Detroit River"],
 	["biodome a la montreal", 6, "Montreal's Biodome - Wonder of the World"],
 	["river rapids", 6, "River Rapids"],
-	// ["mercury filter truck approaching sunset",6, "SPECIAL FILTER SUNSET ON ROAD"],
+	["mercury filter truck approaching sunset",6, "SPECIAL FILTER SUNSET ON ROAD"],
 	["remarkable colored sun", 6, "STUNNING SUNSET IN PARIS ONTARIO WITH TREES IN FOREGROUND"],
 	["view along the river", 6, "View Along the Grand River"],
 	["willow tree in waiting", 6, "willow tree in waiting"],
@@ -166,13 +195,11 @@ export const displaySettingsInitial = [
 	["rail bridge and water lock", 6, "Mini Waterfall with Rail Bridge in Background"],
 
 	// This line
-	//["beachfront restaurant in mexican riviera",6, "Playa Del Carmen Oceanside Restaurant and Beach"],
 	["rain dripping off a giant granite rock face", 6, "RAIN DROPS DRIPPING OFF GIANT ROCK IN TROPICAL FOREST"],
 	["chinese river flowing under a bridge", 6, "River Flowing Under Bridge in Shanghai China"],
 	["trickling stream in bruce peninsula park", 6, "SINGING SANDS TRIBUATARY FROM GEORGIAN BAY - VERY COLD WATER!"],
 	["resort spa ceiling", 6, "Spa Ceiling"],
 	["busiest international bridge crossing", 6, "STREET LEVEL SHOT OF AMBASSADOR BRIDGE WITH TRUCK CROSSING IN WINDSOR ON"],
-	//removed because of dup ["sunrise in agawa",6, "Sunset in Agawa Canyon Beach"],
 	["remnants of destroyed mill", 6, "TRIBUTE TO A MOTHER IN THE RUINS OF AN OLD MILL ALONG THE GRAND RIVER"],
 	["trout river at singing sands beach", 6, "Trout River at Singing Sands Beach Near Tobermory Ontario"],
 	["view from below the bridge", 6, "UNDERNEATH WINDING SHOT OF AMBASSADOR BRIDGE - BUSIEST INTRNATIONAL BORDER CROSSING IN THE WORLD!"],
@@ -180,8 +207,50 @@ export const displaySettingsInitial = [
 	["agawa waterfall with special filter", 6, "Agawa Canyon Mountainside Waterfall in Northern Ontario with Special Filter"],
 	["french capitol building", 6, "French Government Main Parliament Building in Paris France With Special Night Filter"],
 	["fallen tree in the lake", 6, "Glorious Picture of Fallen Tree in Swamp with Special Filter"],
+
+	["hiram walkers head office building",6,"Hiram Walkers Canadian Club HIstorical Head Office Building"],
+	["mexican oasis and gardens",6,"mexican oasis and gardens"],
+	["agawa mountainside waterfall",6,"Mountainside Waterfall in Northern Ontario"],
+	["mill destroyed on the grand",6,"OLD MILL REMNANTS ON GRAND RIVER IN BACKGROUND"],
+	["old fishing camp on the river",6,"Remnants of Old Fishing Camp Along River in Preston Ontario"],
+	["mill destroyed",6,"REMNANTS OF OLD MILL ON GRAND RIVER"],
+	["leviathan at versailles",6,"Shrubbery Leviathan in Front of On of the Palace"],
+	["black and white river view",6,"SPECIAL FILTER VIEW OF GRAND RIVER AND FOREGROUND FOLIAGE"],
+	["louis the 14th statue in paris france",6,"Stunning Statue of Louis the XIV in French Parliament Square"],
+	["mayan tiki huts",6,"Tiki Shade Huts and Lake in Playa Del Carmen Mexico"],
+	["winter wonderland",6,"Winter Wonderland Trail in Northern National Park"],
+	["canadian house of commons historic architecture",6,"Atrium Architecture in Canadian Parliament Buildings"],
+	["b w of underbelly of ambassdor bridge",6,"Black and White Shot of Amassador BRIDGE"],
+	["estuary at singing sands beach",6,"Estuary From Georgian Bay Leading Inland to Provincial Park"],
+	["majestic ceiling of louvre museum",6,"GORGEOUS PAINTED CEILING AND CHANDELIERS IN ONE OF THE MANY PALACES IN VERSAILLES FRANCE"],
+	["extraordinary stained glass in house of commons",6,"Historic Stained Glass Windows in Canadian House of Commons"],
+	["light tunnel",6,"Light Tunnel"],
+	["trail along the shore",6,"LUSH TREE PARTIALLY OBSTRUCTED VIEW OF RAIL BRIDGE OVER GRAND RIVER"],
+	["central park in autumn",6,"NY Central Park in Spring Time"],
+	["rustic barn",6,"Old Barn in Abandoned Backyard"],
+	["rocky shoreline of bruce peninsula",6,"Rocky Shoreline Beach On Georgian Bay"],
+	["pagoda plantation in beijing china",6,"WATERLILLIES WITH CHINESE PAGODA IN BACKDROP IN SHANGHAI CHINA"],
+	["paris france boulevard",6,"Paris France Street View and Amazing Architecture"],
+	["gorgeous cenote on the gulf of mexico",6,"GORGEOUS CENOTE ON THE GULF OF MEXICO"],
+
+	["long shot of rail bridge",6,"Long Shot View of Rail Bridge Over Grand River in Cambridge Ontario"],
+	["picturesque river through the trees",6,"SPECIAL FILTER FRAMED VIEW OF GRAND RIVER"],
+	["wooden bridge along water",6,"Amazing Wooden Walking Trail Along Grand River"],
+	["arc de triomphe",6,"Arc de Triomphe in Paris"],
+	["la louvre museum painted art ceiling",6,"INCREDIBLE SHOT OF PAINTED CEILING OF THE LOUVRE ART MUSEUM THAT HOUSES LA MONA LISA"],
+	["under the base of the famous eiffel tower in paris",6,"Shot up the Middle of the Eiffel Tower at Night in Paris"],
+	["the grotto",6,"Entrance to Grotto Cave in Tobermory"],
+	["new york subway",6,"NY Subway Track 22"],
+	["rail bridge with train",6,"TRAIN CROSSING RAIL BRIDGE OVER GRAND RIVER"],
+	["old log dump side of the grotto",6,"The Other Side of the Grotto in Tobermory"],
+	["the eiffel tower lit at night in paris france",6,"One of a Kind pictures of Paris France's Eiffel Tower Lighted at Night From Behind Security Barrier"],
+	["beachfront restaurant in mexican riviera",6,"Playa Del Carmen Oceanside Restaurant and Beach"],
+	["centuries old highschool two",6,"Hogwarts Lookalike GCI School with filter"],
+	["sunrise in agawa two",6,"Sunset in Agawa Canyon Beach"],
 ]
 
+
+console.log("lenggadhlakdlj: %s",displaySettingsInitial.length)
 
 const capitalizeFirstLetter = function (word){
 	return word.charAt(0).toUpperCase() + word.slice(1)
