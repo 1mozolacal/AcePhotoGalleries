77ee216339a7b5258bfecaf6f94f0329dd6c13c0
Duc Nguyen <nahz3112@gmail.com>
2021-08-27 11:56:42 -0400

    Added styling

--- src/pages/uploads.js
+++ src/pages/uploads.js
@@ -1,25 +1,29 @@
 import React, { useState } from "react";
 import Input from '@material-ui/core/Input';
 import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container'
 import {uploadFileToBlob} from '../utils/azureStorage';
 
 import "../stylesheets/home.sass"
 
 const Uploads = () => {
-
+    const style = {
+        color: "white"
+    }
     const [fileSelected, setFileSelected] = useState()
 
     const uploadFile = async () => {
-
         await uploadFileToBlob(fileSelected)
+        setFileSelected(null)
     }
 
     return (
-        <div>
-            <h1>Upload</h1>
-            <Input label="Update file here" type="file" onChange={(e) => setFileSelected(e.target.files[0])} />
+        <Container>
+            <h1>Upload new photo</h1>
+            <Input label="Update file here" type="file" onChange={(e) => setFileSelected(e.target.files[0])} style={style} />
+            <hr/>
             <Button variant="contained" className="grid-button" type="submit" onClick={uploadFile}>Upload</Button>
-        </div>
+        </Container>
     );
 }
 
